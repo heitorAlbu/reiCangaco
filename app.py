c5a98bde7c1997d082247ef2624619388f2dfa85
Heitor Albuquerque <heitor.albuquerque@gmail.com>
2020-11-15 11:31:30 -0300

    implementacao sqlAlchemy e parte do crud de Usuario

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
 from flask import Flask, render_template, request,  session, flash, url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.secret_key = 'reiDoCangaco'
+#engine = create_engine('mysql://root:12345@localhost/db_rei')
 
-class Usuario:
-    def __init__(self,  login, senha):
-        self.login = login
-        self.senha = senha
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.master' 
+db = SQLAlchemy(app)
 
-usuario1 = Usuario('heitor','12345')
-usuario2 = Usuario('ricardo','54321')
-usuarios = { usuario1.login: usuario1, 
-             usuario2.login: usuario2}
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True, index=True)
+    senha = db.Column(db.String(100), nullable=False)
+    endereco = db.Column(db.String(100), nullable=True)
+    telefone = db.Column(db.String(20), nullable=True)
+    cpf = db.Column(db.String(11), nullable=False)
+    tipo = db.Column(db.Integer, nullable=False)
+
+    def __str__(self):
+        return self.nome
+
+db.create_all()
+
+if __name__ == "__name__":
+    app.run(debug=True)
+
+@app.route('/usuarioList')
+def usuarioList():
+    #if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        #return redirect(url_for('login', proxima=url_for('produtoForm')))
+
+    usuarios = Usuario.query.all()
+    return render_template('usuarioList.html', usuarios = usuarios)
+
+@app.route('/usuario/delete/<int:id>')
+def usuarioDelete(id):
+    usuario = Usuario.query.filter_by(id=id).first()
+    db.session.delete(usuario)
+    db.session.commit()
+    usuarios = Usuario.query.all()
+    return redirect(url_for('usuarioList'))
 
 @app.route('/produtoForm')
 def produtoForm():
@@ -29,6 +61,7 @@ def produtoForm():
     #descricao = request.form['descricao']
     # unid = request.form['unid']
     #produto = Produto(nome,preco, descricao, unid)
+
 @app.route('/produtoList')
 def produtoList():
     return render_template('produtoList.html')
@@ -47,6 +80,7 @@ def login():
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
+    usuarios = Usuario.query.all()
     if request.form['usuario'] in usuarios:
         usuario = usuarios[request.form['usuario']]
         if usuario.senha == request.form['senha']:
